6078ae5055301e3eca41f192f1a1eda1de78872f
Glad Chukwu <nellg.gc@gmail.com>
2024-04-25 08:46:35 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// const btnNavEl = document.querySelector(".btn-mobile--nav");
+// const headerEl = document.querySelector("header");
+
+// btnNavEl.addEventListener("click", function () {
+//   headerEl.classList.toggle("nav-open");
+// });
+
+
+
+
+// const menuToggle = document.getElementById("menu-toggle");
+// const  nav = document.getElementById("main-nav");
+
+// menuToggle.addEventListener("click",() => {
+//     nav.classList.toggle("show");
+// });
